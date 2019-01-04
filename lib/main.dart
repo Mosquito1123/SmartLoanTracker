6c735aeab25ae281ce99a7225d1cd7090a570bac
Hareesh Polla <hpolla@atb.com>
2019-01-04 07:16:33 -0700

    bloc pattern, loan edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
 
 import 'src/screens/dashboard_page.dart';
+import 'src/screens/loans_list_page.dart';
+import 'src/screens/loan_edit_page.dart';
+import './src/bolcs/loan_items_bloc.dart';
+import './src/bolcs/application_bloc.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'LoanTracker',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider(
+      bloc: ApplicationBloc(),
+      child: BlocProvider(
+        bloc: LoanItemsBloc(),
+        child: MaterialApp(
+          title: 'LoanTracker',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          // home: LoansListPage(),
+          routes: {
+            '/': (context) => DashboardPage(),
+            '/loans': (context) => LoansListPage(),
+            // '/loan': (context) => LoanEditPage(),
+          },
+        ),
       ),
-      home: DashboardPage(),
     );
   }
 }
--- lib/src/screens/dashboard_page.dart
+++ lib/src/screens/dashboard_page.dart
@@ -1,44 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../bolcs/loan_items_bloc.dart';
+import '../widgets/side_drawer.dart';
 
 class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('LoanTracker'),
+        title: Text('Dashboard'),
       ),
-      body: LoanList(),
-    );
-  }
-}
-
-class LoanList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance
-          .collection('loan-tracker')
-          .document('LoanTrackerDoc')
-          .collection('loans')
-          .snapshots(),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
-        switch (snapshot.connectionState) {
-          case ConnectionState.waiting:
-            return new Text('Loading...');
-          default:
-            return new ListView(
-              children:
-                  snapshot.data.documents.map((DocumentSnapshot document) {
-                return new ListTile(
-                  title: new Text(document['title']),
-                  subtitle: new Text(document['amount'].toString()),
-                );
-              }).toList(),
-            );
-        }
-      },
+      body: Text('Dashboard Content'),
+      drawer: SideDrawer(),
     );
   }
 }
